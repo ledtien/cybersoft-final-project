fd6c972895b1bc9a1eddb6771ebb99794bb5c50c
Tien <ldtien.1993@gmail.com>
2022-05-02 17:40:32 +0700

    add base fiverr project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy, Fragment } from "react";
+import { createBrowserHistory } from "history";
+import { Route, Router, Switch } from "react-router-dom";
+import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
+import HomePage from "./pages/HomePage/HomePage";
+import Contact from "./pages/Contact/Contact";
+import News from "./pages/News/News";
+import Detail from "./pages/Detail/Detail";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import { UserTemplate } from "./templates/UserTemplate/UserTemplate";
+import CheckoutTemplate from "./templates/CheckoutTemplate/CheckoutTemplate";
+import Checkout from "./pages/Checkout/Checkout";
+import Loading from "./components/Loading/Loading";
+import Profile from "./pages/Profile/Profile";
+import { AdminTemplate } from "./templates/AdminTemplate/AdminTemplate";
+import Dashboard from "./pages/Admin/Dashboard/Dashboard";
+import Films from "./pages/Admin/Films/Films";
+import Showtime from "./pages/Admin/Showtime/Showtime";
+import AddFilm from "./pages/Admin/Films/AddFilm/AddFilm";
+import EditFilm from "./pages/Admin/Films/EditFilm/EditFilm";
+import "./App.css";
+
+export const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment className="App">
+      <Router history={history}>
+        <Loading />
+        <Switch>
+          <HomeTemplate path="/" exact Component={HomePage} />
+          <HomeTemplate path="/home" exact Component={HomePage} />
+          {/* <HomeTemplate path="/contact" exact Component={Contact} />
+        <HomeTemplate path="/news" exact Component={News} />
+        <HomeTemplate path="/detail/:id" exact Component={Detail} />
+        <HomeTemplate path="/profile" exact Component={Profile} />
+        <CheckoutTemplate path="/checkout/:id" exact Component={Checkout} />
+        <UserTemplate path="/login" exact Component={Login} />
+        <UserTemplate path="/register" exact Component={Register} />
+        <AdminTemplate path="/admin" exact Component={Dashboard} />
+        <AdminTemplate path="/admin/users" exact Component={Dashboard} />
+        <AdminTemplate path="/admin/films" exact Component={Films} />
+        <AdminTemplate path="/admin/films/add" exact Component={AddFilm} />
+        <AdminTemplate
+          path="/admin/films/edit/:id"
+          exact
+          Component={EditFilm}
+        />
+        <AdminTemplate
+          path="/admin/films/showtime/:id"
+          exact
+          Component={Showtime}
+        /> */}
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
+import "./index.css";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { DOMAIN_MOVIE } from "./utils/settings/config";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const signalR = require("@microsoft/signalr");
+
+// export const connection = new signalR.HubConnectionBuilder()
+//   .withUrl(`${DOMAIN_MOVIE}/DatVeHub`)
+//   .configureLogging(signalR.LogLevel.Information)
+//   .build();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// connection
+//   .start()
+//   .then(() => {
+
+//   })
+//   .catch((errors) => {
+//     console.log(errors);
+//   });
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
