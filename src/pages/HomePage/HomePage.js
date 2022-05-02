2cb4a36c176eda08447bc931ddd11551afad9782
Tien <ldtien.1993@gmail.com>
2022-05-02 17:44:04 +0700

    add homepage component

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import HomeCarousel from "../../components/HomeCarousel/HomeCarousel";
+
+export default function HomePage() {
+  return (
+    <Fragment>
+      <HomeCarousel />
+    </Fragment>
+  );
+}
