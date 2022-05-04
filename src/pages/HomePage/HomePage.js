c2afdcb4f9a014e8c6c28a92ee0f2f55b8a16d94
Tien <ldtien.1993@gmail.com>
2022-05-04 11:11:56 +0700

    add introduction content

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import CategorySlick from "../../components/CategorySlick/CategorySlick";
 import HomeCarousel from "../../components/HomeCarousel/HomeCarousel";
+import YoutubeEmbed from "../../components/YoutubeEmbed/YoutubeEmbed";
+import { CheckCircleOutlined } from "@ant-design/icons";
 
 export default function HomePage() {
   return (
     <Fragment>
       <HomeCarousel />
-      <div className="bg-gray-100">
+      <div className="bg-gray-100 ">
         <div
           className="flex justify-center items-center px-12 container mx-auto"
           style={{ height: "95px" }}
@@ -53,6 +55,113 @@ export default function HomePage() {
         </div>
       </div>
       <CategorySlick />
+      <div style={{ backgroundColor: "#f1fdf7" }} className="py-24">
+        <div className="grid grid-cols-2 px-12 container mx-auto items-center">
+          <div className=" pr-40">
+            <h1 className="font-bold text-4xl text-gray-700 mb-7">
+              {" "}
+              A whole world of freelance talent at your fingertips
+            </h1>
+            <ul>
+              <li>
+                <div className="mb-2">
+                  <span>
+                    <CheckCircleOutlined
+                      style={{
+                        color: "rgb(122,125,133)",
+                        fontSize: "28px",
+                        marginRight: "10px",
+                        fontWeight: "bold",
+                        position: "relative",
+                        top: "-2px",
+                      }}
+                    />
+                  </span>
+                  <h3 className="inline text-gray-700 font-bold text-lg">
+                    The best for every budget{" "}
+                  </h3>
+                </div>
+                <p className="text-lg text-gray-600">
+                  Find high-quality services at every price point. No hourly
+                  rates, just project-based pricing.
+                </p>
+              </li>
+              <li>
+                <div className="mb-2">
+                  <span>
+                    <CheckCircleOutlined
+                      style={{
+                        color: "rgb(122,125,133)",
+                        fontSize: "28px",
+                        marginRight: "10px",
+                        fontWeight: "bold",
+                        position: "relative",
+                        top: "-2px",
+                      }}
+                    />
+                  </span>
+                  <h3 className="inline text-gray-700 font-bold text-lg">
+                    Quality work done quickly
+                  </h3>
+                </div>
+                <p className="text-lg text-gray-600">
+                  Find the right freelancer to begin working on your project
+                  within minutes.
+                </p>
+              </li>
+              <li>
+                <div className="mb-2">
+                  <span>
+                    <CheckCircleOutlined
+                      style={{
+                        color: "rgb(122,125,133)",
+                        fontSize: "28px",
+                        marginRight: "10px",
+                        fontWeight: "bold",
+                        position: "relative",
+                        top: "-2px",
+                      }}
+                    />
+                  </span>
+                  <h3 className="inline text-gray-700 font-bold text-lg">
+                    Protected payments, every time
+                  </h3>
+                </div>
+                <p className="text-lg text-gray-600">
+                  Always know what you'll pay upfront. Your payment isn't
+                  released until you approve the work.
+                </p>
+              </li>
+              <li>
+                <div className="mb-2">
+                  <span>
+                    <CheckCircleOutlined
+                      style={{
+                        color: "rgb(122,125,133)",
+                        fontSize: "28px",
+                        marginRight: "10px",
+                        fontWeight: "bold",
+                        position: "relative",
+                        top: "-2px",
+                      }}
+                    />
+                  </span>
+                  <h3 className="inline text-gray-700 font-bold text-lg">
+                    24/7 support
+                  </h3>
+                </div>
+                <p className="text-lg text-gray-600">
+                  Questions? Our round-the-clock support team is available to
+                  help anytime, anywhere.
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <YoutubeEmbed embedId="wMgln5SXI_I" />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
