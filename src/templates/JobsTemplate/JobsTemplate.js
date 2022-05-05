81f0142359cd774205f6984a5219f28786ae3c30
Tien <ldtien.1993@gmail.com>
2022-05-05 21:41:07 +0700

    create jobsTemplate for jobs list

---
+++ src/templates/JobsTemplate/JobsTemplate.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { Route } from "react-router-dom";
+import Footer from "../HomeTemplate/Layout/Footer/Footer";
+import JobsHeader from "./Layout/JobsHeader";
+
+export const JobsTemplate = (props) => {
+  const { Component, ...restProps } = props;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+
+  return (
+    <Route
+      {...restProps}
+      render={(propsRoute) => {
+        return (
+          <>
+            <JobsHeader {...propsRoute} />
+
+            <Component {...propsRoute} />
+            <hr className="mt-20" />
+            <Footer />
+          </>
+        );
+      }}
+    />
+  );
+};
