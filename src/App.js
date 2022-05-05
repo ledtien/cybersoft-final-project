91d2be9691fc7f55276b77793a0ef3e41a6f7767
Tien <ldtien.1993@gmail.com>
2022-05-05 21:50:14 +0700

    delete unused components and add jobslist component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,27 @@
-import React, { Suspense, lazy, Fragment } from "react";
+import React, { Fragment } from "react";
 import { createBrowserHistory } from "history";
-import { Route, Router, Switch } from "react-router-dom";
+import { Router, Switch } from "react-router-dom";
 import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
 import HomePage from "./pages/HomePage/HomePage";
-import Contact from "./pages/Contact/Contact";
-import News from "./pages/News/News";
-import Detail from "./pages/Detail/Detail";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import { UserTemplate } from "./templates/UserTemplate/UserTemplate";
-import CheckoutTemplate from "./templates/CheckoutTemplate/CheckoutTemplate";
-import Checkout from "./pages/Checkout/Checkout";
-import Loading from "./components/Loading/Loading";
-import Profile from "./pages/Profile/Profile";
-import { AdminTemplate } from "./templates/AdminTemplate/AdminTemplate";
-import Dashboard from "./pages/Admin/Dashboard/Dashboard";
-import Films from "./pages/Admin/Films/Films";
-import Showtime from "./pages/Admin/Showtime/Showtime";
-import AddFilm from "./pages/Admin/Films/AddFilm/AddFilm";
-import EditFilm from "./pages/Admin/Films/EditFilm/EditFilm";
+// import Loading from "./components/Loading/Loading";
 import "./App.css";
+import JobsList from "./pages/JobsList/JobsList";
+import { JobsTemplate } from "./templates/JobsTemplate/JobsTemplate";
 
 export const history = createBrowserHistory();
 
 function App() {
   return (
-    <Fragment className="App">
+    <div className="App">
       <Router history={history}>
-        <Loading />
+        {/* <Loading /> */}
         <Switch>
           <HomeTemplate path="/" exact Component={HomePage} />
           <HomeTemplate path="/home" exact Component={HomePage} />
-          {/* <HomeTemplate path="/contact" exact Component={Contact} />
-        <HomeTemplate path="/news" exact Component={News} />
-        <HomeTemplate path="/detail/:id" exact Component={Detail} />
-        <HomeTemplate path="/profile" exact Component={Profile} />
-        <CheckoutTemplate path="/checkout/:id" exact Component={Checkout} />
-        <UserTemplate path="/login" exact Component={Login} />
-        <UserTemplate path="/register" exact Component={Register} />
-        <AdminTemplate path="/admin" exact Component={Dashboard} />
-        <AdminTemplate path="/admin/users" exact Component={Dashboard} />
-        <AdminTemplate path="/admin/films" exact Component={Films} />
-        <AdminTemplate path="/admin/films/add" exact Component={AddFilm} />
-        <AdminTemplate
-          path="/admin/films/edit/:id"
-          exact
-          Component={EditFilm}
-        />
-        <AdminTemplate
-          path="/admin/films/showtime/:id"
-          exact
-          Component={Showtime}
-        /> */}
+          <JobsTemplate path="/jobs/search/:name" exact Component={JobsList} />
         </Switch>
       </Router>
-    </Fragment>
+    </div>
   );
 }
 
