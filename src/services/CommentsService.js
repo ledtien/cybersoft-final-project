a112c796312a204d913ca709ecfeecc959ad0cc9
Tien <ldtien.1993@gmail.com>
2022-05-08 01:00:34 +0700

    add postcomment service

--- src/services/CommentsService.js
+++ src/services/CommentsService.js
@@ -4,6 +4,10 @@ export class CommentsService extends baseService {
   getCommentsByJobs = (jobId) => {
     return this.get(`/api/comments/by-job/${jobId}`);
   };
+
+  postComment = (comment) => {
+    return this.post(`/api/comments`, comment);
+  };
 }
 
 export const commentsService = new CommentsService();
