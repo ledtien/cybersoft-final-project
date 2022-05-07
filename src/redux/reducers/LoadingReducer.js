5cdc90e7d156ff59afda7f484316d98925a9c1b3
Tien <ldtien.1993@gmail.com>
2022-05-08 00:59:06 +0700

    create loading reducer

---
+++ src/redux/reducers/LoadingReducer.js
@@ -0,0 +1,22 @@
+import { DISPLAY_LOADING, HIDE_LOADING } from "../constants/LoadingConstant";
+
+const initialState = {
+  isLoading: false,
+};
+
+export const LoadingReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case DISPLAY_LOADING: {
+      state.isLoading = true;
+      return { ...state };
+    }
+
+    case HIDE_LOADING: {
+      state.isLoading = false;
+      return { ...state };
+    }
+
+    default:
+      return state;
+  }
+};
