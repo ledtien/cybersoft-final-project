4e9e2eae580f7aa4d5673bc299acc88c8723bb3d
Tien <ldtien.1993@gmail.com>
2022-05-09 12:35:56 +0700

    add user constant variable

--- src/redux/constants/UserConstant.js
+++ src/redux/constants/UserConstant.js
@@ -1,3 +1,9 @@
 export const USER_SIGN_IN_ACTION = "USER_SIGN_IN_ACTION";
 
 export const USER_SIGN_UP_ACTION = "USER_SIGN_UP_ACTION";
+
+export const UPDATE_USER = "UPDATE_USER";
+
+export const GET_USER_DETAIL = "GET_USER_DETAIL";
+
+export const UPLOAD_USER_IMAGE = "UPLOAD_USER_IMAGE";
