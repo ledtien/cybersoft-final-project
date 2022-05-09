2aad99867a84483531b9aff31682258931f37e66
Tien <ldtien.1993@gmail.com>
2022-05-09 12:33:55 +0700

    add api getuserdetail upload userimage and updateuser

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -8,6 +8,18 @@ export class UserService extends baseService {
   signInService = (form) => {
     return this.post(`/api/auth/signin`, form);
   };
+
+  updateUser = (id, form) => {
+    return this.put(`/api/users/${id}`, form);
+  };
+
+  getUserDetail = (id) => {
+    return this.get(`/api/users/${id}`);
+  };
+
+  uploadUserImage = (formData) => {
+    return this.post(`/api/users/upload-avatar`, formData);
+  };
 }
 
 export const userService = new UserService();
