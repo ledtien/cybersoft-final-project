60c7665b57251a94eb567b3ecb8976cc1bcf96c1
Tien <ldtien.1993@gmail.com>
2022-05-10 11:24:24 +0700

    fix upload image, change from upload antd to input

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -11,31 +11,24 @@ import {
 } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { getJobsByUser, jobDoneAction } from "../../redux/actions/JobsAction";
-import { USER_LOGIN } from "../../utils/settings/config";
 import _ from "lodash";
 import moment from "moment";
 import ModalProfile from "../../components/ModalProfile/ModalProfile";
-import { history } from "../../App";
-import { Upload, message } from "antd";
-import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
-import { useFormik } from "formik";
+
+import { uploadUserImageAction } from "../../redux/actions/UserAction";
 
 export default function User() {
   const { jobsByUser } = useSelector((state) => state.JobsReducer);
   const { userLogin } = useSelector((state) => state.UserReducer);
-  const [loading, setLoading] = useState(false);
-  const [avatarImg, setAvatarImg] = useState("");
+
+  const [avatarImg, setAvatarImg] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getJobsByUser());
   }, [dispatch]);
 
-  if (!localStorage.getItem(USER_LOGIN)) {
-    history.push("/");
-  }
-
-  console.log({ jobsByUser });
+  console.log({ userLogin });
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -49,41 +42,22 @@ export default function User() {
     setIsModalVisible(false);
   };
 
-  const uploadButton = (
-    <div>
-      {loading ? <LoadingOutlined /> : <PlusOutlined />}
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </div>
-  );
-
-  const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-    if (!isJpgOrPng) {
-      message.error("You can only upload JPG/PNG file!");
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-      message.error("Image must smaller than 2MB!");
-    }
-    return isJpgOrPng && isLt2M;
-  };
-
-  const handleChangeImage = (info) => {
-    if (info.file.status === "uploading") {
-      this.setState({ loading: true });
-      return;
-    }
-    if (info.file.status === "done") {
-      // Get this url from response in real world.
-      console.log({ info });
-    }
+  const handleChangeImage = (e) => {
+    let file = e.target.files[0];
     let formData = new FormData();
-    // if (values.avatar !== null) {
-    //   formData.append("avatar", values.avatar, values.avatar.name);
+    const reader = new FileReader();
+    console.log({ file });
+    if (file.size / 1024 / 1024 < 2) {
+      formData.append("avatar", e.target.files[0], e.target.files[0].name);
+      dispatch(uploadUserImageAction(formData));
 
-    // console.log("avatar", formData.get("avatar"));
-
-    //Gọi api gửi các giá trị formdata về backend xử lý
+      reader.readAsDataURL(file);
+      reader.onload = (e) => {
+        setAvatarImg(e.target.result); //hinh base64
+      };
+    } else {
+      alert("Image Error!!!");
+    }
   };
 
   return (
@@ -92,24 +66,19 @@ export default function User() {
         <div className="grid grid-cols-5 gap-4">
           <div className="col-span-2">
             <div className="flex flex-col justify-center max-w-md p-6 border bg-white sm:px-12 dark:bg-coolGray-900 dark:text-coolGray-100">
-              <Upload
-                name="avatar"
-                listType="picture-card"
-                className="avatar-uploader text-center"
-                showUploadList={false}
-                beforeUpload={beforeUpload}
+              <input
+                type="file"
                 onChange={handleChangeImage}
-              >
-                {userLogin.avatar ? (
-                  <img
-                    src={userLogin.avatar}
-                    alt="avatar"
-                    style={{ width: "100%" }}
-                  />
-                ) : (
-                  uploadButton
-                )}
-              </Upload>
+                accept="image/png, image/jpeg"
+                className="text-center mb-5"
+              />
+              <br />
+
+              <img
+                src={avatarImg === null ? userLogin.avatar : avatarImg}
+                alt={userLogin.name.substring(0, 1)}
+                className="w-36 h-36 mx-auto rounded-full dark:bg-coolGray-500 "
+              />
 
               <div className="space-y-4 text-center divide-y divide-coolGray-700">
                 <div className="my-2 space-y-1">
