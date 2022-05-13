84324b3b96f23a329d672b307a2be4d9d5156043
Tien <ldtien.1993@gmail.com>
2022-05-13 15:46:45 +0700

    add uploadimage, update, delete subtype redux

--- src/redux/actions/SubTypeJobsAction.js
+++ src/redux/actions/SubTypeJobsAction.js
@@ -14,3 +14,65 @@ export const getSubTypeJobsAction = () => {
     }
   };
 };
+
+export const createSubTypeJobAction = (data) => {
+  return async (dispatch) => {
+    try {
+      const result = await subTypeJobsService.createSubTypeJob(data);
+      if (STATUS_CODE.SUCCESS) {
+        console.log(result);
+
+        alert("Success!");
+        await dispatch(getSubTypeJobsAction());
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+};
+
+export const uploadImageSubTypeJobAction = (id, data) => {
+  return async (dispatch) => {
+    try {
+      const result = await subTypeJobsService.uploadImageSubTypeJob(id, data);
+      if (STATUS_CODE.SUCCESS) {
+        console.log(result);
+        alert("Success!");
+        await dispatch(getSubTypeJobsAction());
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+};
+
+export const updateSubTypeJobAction = (id, data) => {
+  return async (dispatch) => {
+    try {
+      const result = await subTypeJobsService.updateSubTypeJob(id, data);
+      if (STATUS_CODE.SUCCESS) {
+        console.log(result);
+
+        alert("Update Success!");
+        await dispatch(getSubTypeJobsAction());
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+};
+
+export const deleteSubTypeJobAction = (id) => {
+  return async (dispatch) => {
+    try {
+      const result = await subTypeJobsService.deleteSubTypeJob(id);
+      if (STATUS_CODE.SUCCESS) {
+        console.log(result);
+        alert("Delete Success!");
+        await dispatch(getSubTypeJobsAction());
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+};
--- src/services/SubTypeJobsService.js
+++ src/services/SubTypeJobsService.js
@@ -4,6 +4,22 @@ export class SubTypeJobsService extends baseService {
   getSubTypeJobs = () => {
     return this.get(`/api/sub-type-jobs`);
   };
+
+  createSubTypeJob = (data) => {
+    return this.post(`/api/sub-type-jobs`, data);
+  };
+
+  uploadImageSubTypeJob = (id, formData) => {
+    return this.post(`/api/sub-type-jobs/upload-image/${id}`, formData);
+  };
+
+  updateSubTypeJob = (id, data) => {
+    return this.put(`/api/sub-type-jobs/${id}`, data);
+  };
+
+  deleteSubTypeJob = (id) => {
+    return this.delete(`/api/sub-type-jobs/${id}`);
+  };
 }
 
 export const subTypeJobsService = new SubTypeJobsService();
