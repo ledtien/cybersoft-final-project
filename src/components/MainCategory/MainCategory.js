7784ba89f5990b35991a03b996e1f2de6aef779c
Tien <ldtien.1993@gmail.com>
2022-05-15 02:09:31 +0700

    add resposive

--- src/components/MainCategory/MainCategory.js
+++ src/components/MainCategory/MainCategory.js
@@ -7,17 +7,17 @@ export default function MainCategory() {
   const { typeJobs } = useSelector((state) => state.TypeJobsReducer);
 
   return (
-    <div className="py-24">
-      <div className="container px-12 mx-auto">
+    <div className="lg:py-24 py-16">
+      <div className="container sm:px-0 lg:px-12 mx-auto">
         <h3 className="text-gray-700 font-bold text-3xl">
           Explore the marketplace
         </h3>
-        <div className="grid grid-cols-5 items-center justify-center mt-10">
+        <div className="grid lg:grid-cols-5 grid-cols-2 sm:grid-cols-3 items-center justify-center mt-10">
           {typeJobs?.slice(3, 12).map((job, index) => {
             return (
               <div
                 href="/"
-                className="flex flex-col justify-center items-center relative main-category-underline mb-7 cursor-pointer"
+                className="flex flex-col justify-center items-center relative main-category-underline lg:mb-5 mb-2 cursor-pointer"
                 key={index}
                 onClick={() => {
                   history.push(`/type-jobs/${job._id}`);
